26f73289d3f1195459e43e49cc70b73a37bb7fa3
geigea84 <geigea84@gmail.com>
2020-10-11 01:09:37 -0500

    discarded excess code from js, tweaked 5 day forecast card alignment

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ to create a search history (local storage with setItem
 (json.stringify), recall with getItem (json.parse)),
 create a dynamic list where the array can plug in,
 use for loop to set max length of array*/
-var getCurrentCity = function() {
+var getWeather = function() {
     var currentCity = document.querySelector("#city-input").value;
 
     var cityLowercase = currentCity.toLowerCase();
@@ -167,42 +167,4 @@ var getCurrentCity = function() {
     cityAndDate.appendChild(cityAndDateEl);
 };
 
-//add days to current date
-/*thoughts here: I could violate DRY and set and append one for every 
-card-header, or I could run an array / push it through a for loop?
-Suggestion was to use the info pulled from the API to populate each date*/
-/*
-for (i = 0; i < 5; i++) {
-    console.log(i);
-};
-
-var nextDay = moment().add(i, "days").format("MM/DD/yyyy");
-console.log(nextDay);
-
-//append five day forecast dates
-var cardHeader = document.querySelector(".card-header");
-var nextDayEl = document.createElement("h5");
-nextDayEl.textContent = nextDay;
-cardHeader.appendChild(nextDayEl);
-*/
-
-/*
-//adding current weather
-var getCurrentWeather = function(city) {
-    //format openweathermap url
-    var apiUrl = "api.openweathermap.org/data/2.5/weather?q=" 
-    + city + "&appid=fd6392cb6a45826e0c436c4c5ccc3644";
-    console.log(apiUrl);
-};
-
-//adding five day forecast
-var fiveDayForecast = function(city) {
-    var apiUrl = "api.openweathermap.org/data/2.5/forecast?q="
-    + city + "&appid=fd6392cb6a45826e0c436c4c5ccc3644";
-    console.log(apiUrl);
-};
-*/
-
-searchButton.addEventListener("click", getCurrentCity);
-//searchButton.addEventListener("click", getCurrentWeather);
-//searchButton.addEventListener("click", fiveDayForecast);
+searchButton.addEventListener("click", getWeather);
